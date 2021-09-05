68ebf3648c0c6290ae38a825da8173372523594d
rtealwitter <rtealwitter@gmail.com>
2021-09-05 12:02:14 -0400

    restructure

---
+++ approximation.py
@@ -0,0 +1,123 @@
+import toolbox
+import numpy as np
+import itertools
+
+# adapted from https://docs.python.org/3/library/itertools.html#itertools-recipes
+def powerset(iterable):
+    "powerset([1,2,3]) --> (,) (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
+    s = list(iterable)
+    return [i for i in itertools.chain.from_iterable(itertools.combinations(s, r) for r in range(len(s)+1))]
+
+def intersect(S, T):
+    return tuple(sorted(list(set(S).intersection(set(T)))))
+
+def unionize(S, T):
+    return tuple(sorted(list(set(S + T))))
+
+def getchains(current, chain):
+    chains = []
+    for subset in powerset(current):
+        if len(subset) == 0:
+            chains += [[(), current] + chain]
+        elif len(subset) < len(current):
+            chains += getchains(subset, [current]+chain[:])
+    return chains
+
+def chainwrapper(current, chain):
+    chains = getchains(current, chain)
+    return [chain for chain in chains if len(chain)==len(current)+1]
+
+def constraints(subset, f):
+    upperbounds, lowerbounds = [], []
+    for i in range(len(subset)):
+        lowerbounds += [f[subset[:i] + subset[(i+1):]]]
+    for S in powerset(subset):
+        for T in powerset(subset):
+            if unionize(S,T) == subset and S != subset and T != subset and S != () and T != ():                    
+                upperbounds += [f[S] + f[T]]
+    return min(upperbounds), max(lowerbounds)
+
+def newmodular(n, a=1, b=10, verbose=False, label="f"):
+    universe = list(range(1,n+1))
+    values = [a]*n #np.random.randint(a,b,n)
+    f = {():0}
+    for subset in powerset(universe):
+        total = 0
+        for item in subset:
+            total += values[item-1]
+        f[subset] = total
+        if verbose: print("subset, {}[subset]".format(label), subset, f[subset])
+    def function(a): return f[a]
+    return function
+
+def newsubadditive(n,a=1,b=3, verbose=False, label="f"):
+    universe = list(range(1,n+1))
+    f = {():0}
+    for subset in powerset(universe):
+        if len(subset) == 1: # singleton
+            f[subset] = np.random.randint(a,b)
+        elif len(subset) > 1:
+            upperbound, lowerbound = constraints(subset, f)
+            if upperbound < lowerbound: print('problem!')
+            f[subset] = np.random.randint(lowerbound, upperbound)
+        if verbose: print("subset, {}[subset]".format(label), subset, f[subset])
+    def function(a): return f[a]
+    return function
+
+def newsubmodular(n,a=1,b=10):
+    record = np.full((2**n,n+1), np.Inf)
+    for i in range(n): record[i+1,0] = record[i+1,i+1] = np.random.uniform(a,b)
+    powersets = powerset(list(range(1,n+1)))
+    lastswitch = 0
+    for i in range(n+1,2**n):
+        if len(powersets[i]) > len(powersets[i-1]): lastswitch=i-1
+        subset=powersets[i]
+        print(subset,i,lastswitch)
+        #print(bin(i)[2:].zfill(n))
+    print(record)
+
+def checkproperties(n, f, verbose=False):
+    properties = {"monotone":True, "subadditive":True, "submodular":True}
+    universe = list(range(1,n+1))
+    for S in powerset(universe):
+        for T in powerset(universe):            
+            if set(T).issubset(set(S)) and f(T) > f(S): # monotone
+                if verbose:
+                    print("Not monotone:")
+                    print("T, S", T, S)
+                    print("f[T], f[S]", f(T), f(S))
+                properties["monotone"] = False
+            if f(T) + f(S) < f(unionize(S,T)): # subadditive
+                if verbose:
+                    print("Not subadditive:")
+                    print("T, S, union", T, S, unionize(S,T))
+                    print("f[T], f[S], f[union]", f(T), f(S), f(unionize(S,T)))
+                properties["subadditive"] = False
+            if f(T) + f(S) < f(unionize(S,T)) + f(intersect(S,T)): # submodular
+                if verbose:
+                    print("Not submodular:")
+                    print("T, S, union, intersect(S,T)", T, S, unionize(S,T), intersect(S,T))
+                    print("f[T], f[S], f[union], f[intersect(S,T)]", f(T), f(S), f(unionize(S,T)), f(intersect(S,T)))
+                properties["submodular"] = False
+    return properties
+
+def solvemsop(n, c, u):
+    chains = chainwrapper(tuple(range(1,n+1)),[])
+    objectives = []
+    for chain in chains:
+        objectives += [toolbox.msop(c, u, chain)]
+    minimum = min(objectives)
+    minimumchain = chains[objectives.index(minimum)]
+    return minimum, minimumchain
+
+if __name__ == '__main__':
+    n = 4
+    verbose = True
+    cost = newmodular(n, label='c', verbose=verbose)
+    utility = newsubadditive(n, label='u', verbose=verbose)
+    minobj, minchain = solvemsop(n, cost, utility)
+    localobj = toolbox.local(cost, utility, n, toolbox.insert, verbose=verbose)
+    greedyobj = toolbox.greedy(cost, utility, n)
+    print(localobj/minobj)
+    print(greedyobj/minobj)
+    
--- toolbox.py
+++ toolbox.py
@@ -1,7 +1,4 @@
-import itertools
 import numpy as np
-import random
-import matplotlib.pyplot as plt
 
 def tochain(ordering):
     subset = ()
@@ -24,172 +21,58 @@ def unique(ordering):
             new += [i]
     return new
 
-# adapted from https://docs.python.org/3/library/itertools.html#itertools-recipes
-def powerset(iterable):
-    "powerset([1,2,3]) --> (,) (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
-    s = list(iterable)
-    return [i for i in itertools.chain.from_iterable(itertools.combinations(s, r) for r in range(len(s)+1))]
-
-def intersect(S, T):
-    return tuple(sorted(list(set(S).intersection(set(T)))))
-
-def unionize(S, T):
-    return tuple(sorted(list(set(S + T))))
-
-def constraints(subset, f):
-    upperbounds, lowerbounds = [], []
-    for i in range(len(subset)):
-        lowerbounds += [f[subset[:i] + subset[(i+1):]]]
-    for S in powerset(subset):
-        for T in powerset(subset):
-            if unionize(S,T) == subset and S != subset and T != subset and S != () and T != ():                    
-                upperbounds += [f[S] + f[T]]
-    return min(upperbounds), max(lowerbounds)
-
-def newmodular(n, a=1, b=10, verbose=False, label="f"):
-    universe = list(range(1,n+1))
-    values = [a]*n #np.random.randint(a,b,n)
-    f = {():0}
-    for subset in powerset(universe):
-        total = 0
-        for item in subset:
-            total += values[item-1]
-        f[subset] = total
-        if verbose: print("subset, {}[subset]".format(label), subset, f[subset])
-    def function(a): return f[a]
-    return function
-
-def newsubadditive(n,a=1,b=3, verbose=False, label="f"):
-    universe = list(range(1,n+1))
-    f = {():0}
-    for subset in powerset(universe):
-        if len(subset) == 1: # singleton
-            f[subset] = np.random.randint(a,b)
-        elif len(subset) > 1:
-            upperbound, lowerbound = constraints(subset, f)
-            if upperbound < lowerbound: print('problem!')
-            f[subset] = np.random.randint(lowerbound, upperbound)
-        if verbose: print("subset, {}[subset]".format(label), subset, f[subset])
-    def function(a): return f[a]
-    return function
-
-def newsubmodular(n,a=1,b=10):
-    record = np.full((2**n,n+1), np.Inf)
-    for i in range(n): record[i+1,0] = record[i+1,i+1] = np.random.uniform(a,b)
-    powersets = powerset(list(range(1,n+1)))
-    lastswitch = 0
-    for i in range(n+1,2**n):
-        if len(powersets[i]) > len(powersets[i-1]): lastswitch=i-1
-        subset=powersets[i]
-        print(subset,i,lastswitch)
-        #print(bin(i)[2:].zfill(n))
-    print(record)
-
-def checkproperties(n, f, verbose=False):
-    properties = {"monotone":True, "subadditive":True, "submodular":True}
-    universe = list(range(1,n+1))
-    for S in powerset(universe):
-        for T in powerset(universe):            
-            if set(T).issubset(set(S)) and f(T) > f(S): # monotone
-                if verbose:
-                    print("Not monotone:")
-                    print("T, S", T, S)
-                    print("f[T], f[S]", f(T), f(S))
-                properties["monotone"] = False
-            if f(T) + f(S) < f(unionize(S,T)): # subadditive
-                if verbose:
-                    print("Not subadditive:")
-                    print("T, S, union", T, S, unionize(S,T))
-                    print("f[T], f[S], f[union]", f(T), f(S), f(unionize(S,T)))
-                properties["subadditive"] = False
-            if f(T) + f(S) < f(unionize(S,T)) + f(intersect(S,T)): # submodular
-                if verbose:
-                    print("Not submodular:")
-                    print("T, S, union, intersect(S,T)", T, S, unionize(S,T), intersect(S,T))
-                    print("f[T], f[S], f[union], f[intersect(S,T)]", f(T), f(S), f(unionize(S,T)), f(intersect(S,T)))
-                properties["submodular"] = False
-    return properties
-
-def getchains(current, chain):
-    chains = []
-    for subset in powerset(current):
-        if len(subset) == 0:
-            chains += [[(), current] + chain]
-        elif len(subset) < len(current):
-            chains += getchains(subset, [current]+chain[:])
-    return chains
-
-def chainwrapper(current, chain):
-    chains = getchains(current, chain)
-    return [chain for chain in chains if len(chain)==len(current)+1]
-
-def msop(fm, gm, chain):
+def msop(c, u, chain):
     obj = 0
     for i in range(len(chain)):
         current, previous = chain[i], ()
         if i > 0: previous = chain[i-1]
-        obj += fm(current) * (gm(current) - gm(previous))
+        obj += c(current) * (u(current) - u(previous))
     return obj
 
-def solvemsop(n, fm, gm):
-    chains = chainwrapper(tuple(range(1,n+1)),[])
-    objectives = []
-    for chain in chains:
-        objectives += [msop(fm, gm, chain)]
-    minimum = min(objectives)
-    minimumchain = chains[objectives.index(minimum)]
-    return minimum, minimumchain
+def move(ordering, i, j):
+    return ordering[:j] + [ordering[i]] + ordering[j:i] + ordering[i+1:]
 
-def density(fd, gd, candidate, start=()):
-    if candidate == () and start != (): # exclude case where candidate is empty set
-        return np.Inf
-    if not set(start) < set(candidate): # ensure set is subset of candidate
-        return np.Inf
-    if gd(candidate) == gd(start): # prevent division by 0
-        return np.Inf
-    return (fd(candidate)-fd(start))/(gd(candidate)-gd(start))
+def insert(ordering, i, j):
+    return ordering[:j] + [ordering[i]] + ordering[j:]
 
-def solvedensity(n, fd, gd, start=()):
-    objectives = [np.Inf]
-    for candidate in powerset(range(1,n+1)):
-        if candidate != ():
-            objectives += [density(fd, gd, candidate, start)]
-    minimum = min(objectives)
-    minimumcandidate = powerset(range(1,n+1))[objectives.index(minimum)]
-    if minimum == np.Inf:
-        minimumcandidate = tuple(range(1,n+1))
-    return minimum, minimumcandidate
+def swap(ordering, i, j):
+    return ordering[:j] + [ordering[i]] + ordering[j+1:i] + [ordering[j]] + ordering[i+1:]
 
-def sml(fs, gs, lowerbound, candidate):
-    if gs(candidate) < lowerbound:
-        return np.Inf
-    return fs(candidate)
+def local(c, u, n, method, label='', verbose=False):
+    ordering = list(range(1,n+1))
+    obj = msop(c, u, tochain(ordering))
+    improved = True
+    while improved:
+        improved = False
+        bestordering = ordering
+        for i in list(range(n)):
+            for j in range(i):
+                new = method(ordering, i, j)
+                objnew = msop(c, u, tochain(new))
+                if objnew < obj: 
+                    obj = objnew
+                    bestordering = unique(new)
+                    improved = True
+        ordering = bestordering
+    return msop(c, u, tochain(ordering))
 
-def solvesml(n, fs, gs, lowerbound):
-    objectives = []
-    for candidate in powerset(range(1,n+1)):
-        objectives += [sml(fs, gs, lowerbound, candidate)]
-    minimum = min(objectives)
-    minimumcandidate = powerset(range(1,n+1))[objectives.index(minimum)]
-    return minimum, minimumcandidate
-
-def plot(x, subtitle="", file=""):
-    plt.hist(x)
-    plt.suptitle("Histogram of Reduction/Optimal")
-    plt.title(subtitle)
-    plt.ylabel("Count")
-    plt.xlabel("Reduction/Optimal")
-    if file != "":
-        plt.savefig(file)
-    else:
-        plt.show()
+def greedy(c, u, n):
+    remaining = list(range(1,n+1))
+    ordering = []
+    while len(ordering) < n:
+        maxitem = 0
+        maxratio = np.Inf
+        for item in remaining:
+            candidate = tuple(sorted((*ordering,item)))
+            if u(candidate)/c(candidate) < maxratio:
+                maxratio = u(candidate)/c(candidate)
+                maxitem = item
+        ordering += [maxitem]
+        remaining.remove(maxitem)
+    obj = msop(c, u, tochain(ordering))
+    return obj
 
 if __name__ == '__main__':
-    #f = newmodular(3, verbose=True)
-    n = 3
-    chains = chainwrapper(tuple(range(1,n+1)),[])
-    for chain in chains:
-        print(chain)
-
+    pass
 
 
