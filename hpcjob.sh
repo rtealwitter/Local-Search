119255c0b3d24049d220193ee921da3b0dbd4d41
Teal Witter <rtealwitter@gmail.com>
2021-12-15 09:28:03 -0500

    run with n=50

--- hpcjob.sh
+++ hpcjob.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBATCH --time=48:00:00
-#SBATCH --mem=4GB
+#SBATCH --mem=16GB
 #SBATCH --job-name=Local-Search
 #SBATCH --mail-type=END
 #SBATCH --mail-user=rtw262@nyu.edu
